f2871735dfe7b339fbd3ca096f54850e7c9e1370
up1 <somkiat.p@gmail.com>
2024-05-13 23:20:40 +0700

    Try to use mock

--- nodejs/ui/constants/index.ts
+++ nodejs/ui/constants/index.ts
@@ -1,2 +1,2 @@
 export const APP_NAME = 'Blog';
-export const API_BASE_URL = 'https://a1ae2290-56d2-43a8-a93f-8eeba1a91bb5.mock.pstmn.io';
+export const API_BASE_URL = 'https://653da91b-b096-4dbe-933c-0ea1ed02b1f5.mock.pstmn.io';
