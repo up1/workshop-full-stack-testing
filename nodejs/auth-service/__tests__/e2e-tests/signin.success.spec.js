5101fed2111409dbf7e7d7d8e3212b7cf33df3d3
up1 <somkiat.p@gmail.com>
2024-05-13 20:45:02 +0700

    Validate json schema

--- nodejs/auth-service/__tests__/e2e-tests/signin.success.spec.js
+++ nodejs/auth-service/__tests__/e2e-tests/signin.success.spec.js
@@ -19,4 +19,37 @@ test("Success response with jwttoken", async () => {
   expect(response.body.accessToken).toBeTruthy();
   // check response.body.accessToken is jwt format
   expect(response.body.accessToken.split(".").length).toEqual(3);
+  // check format of response.body with jsonschema
+  const schema = {
+    $schema: "http://json-schema.org/draft-07/schema#",
+    type: "object",
+    properties: {
+      id: {
+        type: "integer",
+      },
+      username: {
+        type: "string",
+        pattern: "^[a-zA-Z0-9]*$",
+      },
+      email: {
+        type: "string",
+        format: "email",
+      },
+      roles: {
+        type: "array",
+        items: {
+          type: "string",
+        },
+      },
+      accessToken: {
+        type: "string",
+        pattern: "^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_.+/=]*$",
+      },
+    },
+    required: ["id", "username", "email", "roles", "accessToken"],
+  };
+  const Validator = require("jsonschema").Validator;
+  const v = new Validator();
+  const result = v.validate(response.body, schema);
+  expect(result.valid).toBeTruthy();
 });
