eca5496549892a684399bad37ab8c4b5792ba0a0
up1 <somkiat.p@gmail.com>
2024-11-13 23:52:01 +0700

    Downgrade chai to v4

--- nodejs/product-service/__tests__/bdd/features/product-list.feature
+++ nodejs/product-service/__tests__/bdd/features/product-list.feature
@@ -2,7 +2,7 @@ Feature: Products List
     Scenario: Load the products list
         When we request the products list
         Then we should receive
-            | name           | description            |    price  |
+            |     name        |     description       |    price  |
             | product 1       | product 1 description |    100    |
-            | product 2       | product 1 description |    200    |
-            | product 3       | product 1 description |    300    |
+            | product 2       | product 2 description |    200    |
+            | product 3       | product 3 description |    300    |
--- nodejs/product-service/__tests__/bdd/features/steps/product.steps.js
+++ nodejs/product-service/__tests__/bdd/features/steps/product.steps.js
@@ -1,63 +1,4 @@
-# Workshop :: Product Service
-* Backend = NodeJS
-
-### Steps to run
-```
-cd product-service
-npm install
-node server.js
-```
-
-### List of endpoints
-* http://localhost:3002
-* API documentation
-  * http://localhost:3002/api-docs/
-
-### Testing workshop
-* API testing
-
-### Run all tests
-```
-npm test
-```
-
-### E2E tests
-```
-npm run e2e-test
-```
-
-### Working with BDD (Behavior-Driven Development)
-* [Cucumber with JavaScript](https://cucumber.io/docs/installation/javascript/)
-* [Gherkin reference](https://cucumber.io/docs/gherkin/)
-
-Install
-```
-$npm install cucumber -D
-```
-
-Edit file `package.json`
-```
-"scripts": {
-    
-    "bdd": "cucumber-js **/features/product-list.feature"
-}
-```
-
-Create feature file in `__tests__/bdd/features/product-list.feature`
-```
-Feature: Products List
-    Scenario: Load the products list
-        When we request the products list
-        Then we should receive
-            |     name        |     description       |    price  |
-            | product 1       | product 1 description |    100    |
-            | product 2       | product 2 description |    200    |
-            | product 3       | product 3 description |    300    |
-```
-
-Create steps file in `__tests__/bdd/features/steps/product.steps.js`
-```
-const { When, Then, After } = require("cucumber");
+const { When, Then } = require("cucumber");
 const expect = require("chai").expect;
 const request = require("supertest");
 const app = require("../../../../app");
@@ -104,11 +45,3 @@ Then("we should receive", async (dataTable) => {
     expect(actual.price).eq(parseFloat(expected.price));
   }
 });
-
-```
-
-Run test
-```
-$export ACCESS_TOKEN=<your token>
-$npm run bdd
-```
